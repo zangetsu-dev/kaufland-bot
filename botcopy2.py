cf3313588d2299ebc8d32b8e5dc350b17920ca42
zangetsu-dev <samonii912@gmail.com>
2025-05-05 20:38:41 +0200

    Update botcopy2.py

--- botcopy2.py
+++ botcopy2.py
@@ -9,7 +9,7 @@ from PIL import Image
 from pdf2image import convert_from_path
 
 # === НАСТРОЙКИ ===
-TOKEN = "7466924390:AAF7g-qFpwhdKUTlWv6nclXgMckHu_uaNUo"
+TOKEN = ""
 TESSERACT_PATH = "/usr/bin/tesseract"
 POPPLER_PATH = "/usr/bin"
 
